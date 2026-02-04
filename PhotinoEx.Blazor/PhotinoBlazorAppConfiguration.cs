45308c7ea913444fb1fe024d2063e9a01150208c
CWX <cwxdev@outlook.com>
2026-02-04 20:24:30 +0000

    initial commit

---
+++ PhotinoEx.Blazor/PhotinoBlazorAppConfiguration.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotinoEx.Blazor
+{
+    public class PhotinoBlazorAppConfiguration
+    {
+        public Uri AppBaseUri { get; set; }
+
+        public string HostPage { get; set; }
+    }
+}
---
+++ PhotinoEx.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace PhotinoEx.Core;
+
+public class Class1
+{
+}
