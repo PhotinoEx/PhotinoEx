7f1c399657a7a8be438661587698cda04332b25d
CWX <cwxdev@outlook.com>
2026-02-28 21:37:54 +0000

    add tray and trayIcon class for linux

--- PhotinoEx.Core/Platform/Linux/Tray/LinPhotinoExTray.cs
+++ PhotinoEx.Core/Platform/Linux/Tray/LinPhotinoExTray.cs
@@ -1,6 +1,51 @@
+using System.Collections.Concurrent;
+
 namespace PhotinoEx.Core.Platform.Linux.Tray;
 
-public class LinPhotinoExTray
+public class LinPhotinoExTray : IPhotinoExTray
 {
-    
+    private ConcurrentDictionary<string, IPhotinoExTrayIcon> _iconList { get; set; } = new();
+    private IntPtr _busConnection { get; set; }
+    private int _instanceCount { get; set; }
+
+    public LinPhotinoExTray(IntPtr connection)
+    {
+        _busConnection = connection;
+    }
+
+    public Task<IPhotinoExTrayIcon> CreateTrayIconAsync(string id, string iconPath, string? toolTip = null, object? menu = null)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+        {
+            throw new ArgumentException($"Argument: {nameof(id)} cant be null or whitespace, must be a unique identifier");
+        }
+
+        if (_iconList.ContainsKey(id))
+        {
+            throw new InvalidOperationException($"id: {id} already exists, must be a unique identifier");
+        }
+
+        var trayIcon = new LinPhotinoExTrayIcon(_busConnection, id, iconPath, toolTip, menu, ++_instanceCount);
+
+        _iconList.TryAdd(id, trayIcon);
+        Console.WriteLine($"Created and added {id} to tray");
+
+        return Task.FromResult<IPhotinoExTrayIcon>(trayIcon);
+    }
+
+    public bool TryGetTrayIcon(string id, out IPhotinoExTrayIcon? icon)
+    {
+        return _iconList.TryGetValue(id, out icon);
+    }
+
+    public bool TryRemoveTrayIcon(string id, out IPhotinoExTrayIcon? icon)
+    {
+        return _iconList.TryRemove(id, out icon);
+    }
+
+    public bool TryRemoveAllTrayIcons()
+    {
+        _iconList.Clear();
+        return !_iconList.Any();
+    }
 }
--- PhotinoEx.Core/Platform/Linux/Tray/LinPhotinoExTrayIcon.cs
+++ PhotinoEx.Core/Platform/Linux/Tray/LinPhotinoExTrayIcon.cs
@@ -1,6 +1,47 @@
 namespace PhotinoEx.Core.Platform.Linux.Tray;
 
-public class LinPhotinoExTrayIcon
+public class LinPhotinoExTrayIcon : IPhotinoExTrayIcon
 {
-    
+    private IntPtr _connection { get; set; }
+    private string _id { get; set; }
+    private string _iconPath { get; set; }
+    private string? _toolTip { get; set; }
+    private object? _contextMenu { get; set; }
+    private bool _isVisible { get; set; } = true;
+    private string _busName { get; set; }
+
+    public LinPhotinoExTrayIcon(IntPtr connection, string id, string iconPath, string? toolTip, object? menu, int instance)
+    {
+        _connection = connection;
+        _id = id;
+        _iconPath = iconPath;
+        _toolTip = toolTip;
+        _contextMenu = menu;
+        _busName = $"org.kde.StatusNotifierItem-{Environment.ProcessId}-{instance}";
+    }
+
+    public async Task<IPhotinoExTrayIcon> CreateAsync()
+    {
+        return this;
+    }
+
+    public async Task<bool> DisposeAsync()
+    {
+        return true;
+    }
+
+    public void SetVisibility(bool state)
+    {
+        _isVisible = state;
+    }
+
+    public void SetIconPath(string path)
+    {
+        _iconPath = path;
+    }
+
+    public void SetContextMenu(object menu)
+    {
+        _contextMenu = menu;
+    }
 }
